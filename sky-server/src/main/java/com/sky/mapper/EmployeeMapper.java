6032f7129511a2d31cb9e87b4c320e51934521fe
a1198777132 <13742042+AlexMinhai@user.noreply.gitee.com>
2023-11-24 18:00:36 +0800

    员工分页查询业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,8 @@
 package com.sky.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.commons.lang3.builder.ToStringExclude;
 import org.apache.ibatis.annotations.Insert;
@@ -17,6 +19,13 @@ public interface EmployeeMapper extends BaseMapper <Employee>{
      */
     @Select("select * from sky_take_out.employee where username = #{username}")
     Employee getByUsername(String username);
+
+    /**
+     * 分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
     /**
      * 插入员工数据
